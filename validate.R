2c8b3d2f25c5446d739b9c67397e8ec8110b3e85
noguchit <noguchit@gmail.com>
2017-03-13 14:13:12 -0400

    added validation file

---
+++ validate.R
@@ -0,0 +1,15 @@
+# looks for melt1 objects, it turns 1-5 values, valid values
+source("all.R")
+allValid=melt1
+source("ug.R")
+ugValid=melt1
+source("fsg.R")
+fsgValid=melt1
+source("f.R")
+fValid=melt1
+source("fs.R")
+fsValid=melt1
+source("fug.R")
+fugValid=melt1
+source("fg.R")
+fgValid=melt1
